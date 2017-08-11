3425ec87fab57f2c10ff3440082a57515f7490b8
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-08-11 16:29:09 +0200

    Updated helper script

--- process-kitti-tracking.sh
+++ process-kitti-tracking.sh
@@ -1,36 +1,61 @@
 #!/usr/bin/env bash
-# Computes depth maps using dispnet for the specified KITTI odometry sequence.
-# Puts the generated depth maps in a folder called 'precomputed-depth-dispnet',
-# at the root of the sequence's folder. The files are 32-bit float maps in PFM
+# Computes depth maps using dispnet for the specified KITTI tracking sequence.
+# Puts the generated depth maps in a folder called
+# 'precomputed-depth-dispnet/<sequence_number>',
+# at the root of the dataset's folder. The files are 32-bit float maps in PFM
 # format. Note that most image viewers won't display them correctly, and you
-# need to use a custom library (see GitHub) for loading them in your programs.
+# need to use a custom library (e.g., pfmLib) for loading them into your 
+# programs.
 
 set -eu
 IFS=$'\n\t'
 
-if [[ "$#" -ne 1 ]]; then
-  echo >&2 "Usage: $0 <kitti_sequence_root>"
+if [[ "$#" -ne 2 ]]; then
+  echo >&2 "Usage: $0 <tracking_dataset_root> <sequence_number>"
   exit 1
 fi
 
 SEQUENCE_ROOT="$1"
+SEQUENCE_NUMBER="$2"
+# Zero-Padded Sequence Number
+PSN="$(printf '%04d' ${SEQUENCE_NUMBER})"
+
+printf "Will process sequence [%04d] from dataset located at [%s].\n" \
+  "${SEQUENCE_NUMBER}" "${SEQUENCE_ROOT}"
+
 SCRIPT_DIR="$(pwd)"
 # How many images from the dataset to process.
 LIMIT=100000
 
 cd "$SEQUENCE_ROOT"
+LEFT_DIR="training/image_02/${PSN}"
+RIGHT_DIR="training/image_03/${PSN}"
+OUT_DIR="training/precomputed-depth-dispnet/${PSN}"
 
-mkdir -p precomputed-depth-dispnet
+if ! [[ -d "${LEFT_DIR}" ]]; then
+  echo >&2 "Invalid left image directory."
+  exit 2
+fi
 
-ls image_0/*.png | head -n "$LIMIT" >| left-dispnet-in.txt
-ls image_1/*.png | head -n "$LIMIT" >| right-dispnet-in.txt
+if ! [[ -d "${RIGHT_DIR}" ]]; then
+  echo >&2 "Invalid right image directory."
+  exit 3
+fi
 
-ls image_0/*.png | head -n "$LIMIT" | \
-    sed 's/image_0/precomputed-depth-dispnet/g' | \
+mkdir -vp "$OUT_DIR"
+
+ls "${LEFT_DIR}"/*.png | head -n "$LIMIT" >| left-dispnet-in.txt
+ls "${RIGHT_DIR}"/*.png | head -n "$LIMIT" >| right-dispnet-in.txt
+
+ls "${LEFT_DIR}"/*.png | head -n "$LIMIT" | \
+    sed 's/image_02/precomputed-depth-dispnet/g' | \
     sed 's/png/pfm/g' >| left-dispnet-out.txt
 
-# TODO: how about trying DispNetCorr1D-K?
-${SCRIPT_DIR}/run-network.sh -n DispNet-K -g 0 -v \
+# TODO: how about trying DispNetCorr1D-K? It performs slightly better in their
+# paper thanks to the explicit correlation layer.
+MODEL="DispNet-K"
+
+${SCRIPT_DIR}/run-network.sh -n "${MODEL}" -g 0 -vvv \
   left-dispnet-in.txt right-dispnet-in.txt left-dispnet-out.txt
 
 
